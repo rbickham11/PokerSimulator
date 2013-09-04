719e0db6b1ed5209872cfe89f94ad36289d37cab
rbickham <rcbickha@svsu.edu>
2013-09-04 19:21:54 -0400

    Fixed two pair order issue

--- PokerSimulator/WinnerChecker.cs
+++ PokerSimulator/WinnerChecker.cs
@@ -321,8 +321,8 @@ namespace PokerSimulator
                                     break;
                             dupCard = sevenCardHand[j];
                             sevenCardHand.RemoveRange(j, 2);
-                            sevenCardHand.Insert(4, dupCard);
-                            sevenCardHand.Insert(4, dupCard);
+                            sevenCardHand.Insert(3, dupCard);
+                            sevenCardHand.Insert(3, dupCard);
                         }
                         sevenCardHand.RemoveRange(0, 2);
                         fiveCardHands.AddRange(sevenCardHand);
