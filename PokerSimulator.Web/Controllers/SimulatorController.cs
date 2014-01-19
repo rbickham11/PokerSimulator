c4100be46565f48e634acadea7de5ccd6bcd9275
rbickham11 <rcbickha@svsu.edu>
2014-01-19 02:16:20 -0500

    Added simulation request model, started view/controller

---
+++ PokerSimulator.Web/Controllers/SimulatorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PokerSimulator.Web.Controllers
+{
+    public class SimulatorController : Controller
+    {
+        //
+        // GET: /Simulation/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ PokerSimulator.Web/Models/SimulationRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PokerSimulator.Web.Models
+{
+    public class SimulationRequestModel
+    {
+        public IEnumerable<int> SetHands { get; set; }
+        public int RandomHands { get; set; }
+        public IEnumerable<int> SetBoard { get; set; }
+        public int HandsToSimulate { get; set; }
+        public bool RandomChange { get; set; }
+    }
+}
