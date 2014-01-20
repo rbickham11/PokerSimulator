5b8345153c2fd564a38f7cfaea294d01e2591b4c
rbickham11 <rcbickha@svsu.edu>
2014-01-20 03:10:51 -0500

    Created simple page to test simulation request parameters

--- PokerSimulator.Web/Controllers/SimulatorController.cs
+++ PokerSimulator.Web/Controllers/SimulatorController.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using PokerSimulator.Web.Models;
+using PokerSimulator.Lib;
+
 namespace PokerSimulator.Web.Controllers
 {
     public class SimulatorController : Controller
     {
         //
-        // GET: /Simulation/
+        // GET: /Simulator/
         public ActionResult Index()
         {
             return View();
+        }
+
+        //POST: /Simulator/
+        [HttpPost]
+        public ActionResult Index(SimulationRequestModel request)
+        {
+            return View(request);
         }
 	}
 }
--- PokerSimulator.Web/Models/SimulationRequestModel.cs
+++ PokerSimulator.Web/Models/SimulationRequestModel.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+using PokerSimulator.Lib;
 
 namespace PokerSimulator.Web.Models
 {
     public class SimulationRequestModel
     {
         public IEnumerable<int> SetHands { get; set; }
+        [Required]
+        [Display(Name=("Random Hands"))]
         public int RandomHands { get; set; }
         public IEnumerable<int> SetBoard { get; set; }
+        [Required] 
+        [Display(Name="Hands to Simulate")]
         public int HandsToSimulate { get; set; }
+        [Display(Name="Change random hands on each simulation?")]
         public bool RandomChange { get; set; }
     }
 }
