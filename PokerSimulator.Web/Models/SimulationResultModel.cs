bfaad614cdf293c7df3acb82b4844269cfe13cd7
rbickham11 <rcbickha@svsu.edu>
2014-01-24 10:59:12 -0500

    Working on models and test page

---
+++ PokerSimulator.Web/Models/SimulationResultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PokerSimulator.Web.Models
+{
+    public class SimulationResultModel
+    {
+        public IEnumerable<int> SetHands { get; set; }
+        public IEnumerable<int> PlayerWinCounts { get; set; }
+        public IEnumerable<int> RankWinCounts { get; set; }
+        public IEnumerable<SimulatedHandModel> SimulatedHands { get; set; }
+    }
+}
---
+++ PokerSimulator.Web/Scripts/Simulator.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var handCount = 1;
+    $("#addHand").click(function () {
+        handCount++;
+        var html = "<label for='SetHands' class='control-label col-md-5'>Hand #" + handCount + ": </label>"
+            + "<input style='width:100px;' type='number' class='text-box single-line' name='SetHands' />"
+            + "<input style='width:100px;' type='number' class='text-box single-line' name='SetHands' /><br />";
+        $("#hands_form").append(html);
+    });
+});
