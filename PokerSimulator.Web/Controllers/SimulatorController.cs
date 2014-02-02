911866e09bce7fdd611a2106013808389ea739da
rbickham11 <rcbickha@svsu.edu>
2014-02-02 15:41:29 -0500

    Removed unused usings

--- PokerSimulator.Web/Controllers/SimulatorController.cs
+++ PokerSimulator.Web/Controllers/SimulatorController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 
 using PokerSimulator.Web.Models;
 using PokerSimulator.Lib;
-using System.Diagnostics;
 
 namespace PokerSimulator.Web.Controllers
 {
--- PokerSimulator.Web/Models/SimulationRequestModel.cs
+++ PokerSimulator.Web/Models/SimulationRequestModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
-using PokerSimulator.Lib;
 
 namespace PokerSimulator.Web.Models
 {
